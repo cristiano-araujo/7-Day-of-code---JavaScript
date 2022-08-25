042841407481009a637c828d42cd99dfda2b52d5
Cristiano112715 <cristiano112715@gmail.com>
2022-08-24 22:51:25 -0300

    atualização do javascript

--- 2º dia/script.js
+++ 2º dia/script.js
@@ -68,7 +68,7 @@ alert(msg);
 Teste os códigos acima e tente adaptá-los ao seu programa!
 */
 
-
+/*
 
 - Qual o seu nome?
 - Quantos anos você tem?
@@ -88,4 +88,20 @@ if (resposta == 1){
 }
 if (resposta == 2){
     // dê a resposta negativa
+}
+*/
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+const gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 - SIM ou 2 - NÃO`);
+if (gosta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}
+if (gosta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
 }
