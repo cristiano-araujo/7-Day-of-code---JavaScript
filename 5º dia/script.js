aea1a8aeaa82fe73179364c3dbccb8bb9407acbc
Cristiano112715 <cristiano112715@gmail.com>
2022-08-26 23:27:58 -0300

    Atualização da pasta 5º dia de desafio

--- 5º dia/script.js
+++ 5º dia/script.js
@@ -1,45 +1,56 @@
-/*  Você já brincou de tentar adivinhar o número que seu amigo ou amiga estava pensando? Hoje você vai voltar na infância e fazer exatamente isso. Mas agora, o jogo vai ser contra o próprio computador!
+/* Mais um dia e mais um desafio no #7DaysOfCode!
 
-Você deve criar um programinha que comece com um valor específico pré-definido entre 0 a 10 para o número que você vai adivinhar (7, por exemplo).
+Agora você já está bem mais perto de finalizar esta jornada e de ter um novo projeto no seu portfólio.
 
-Em seguida, o programa vai perguntar para você qual o valor que você deseja chutar e, caso você acerte, ele irá te parabenizar. Caso erre, ele vai te dar mais 2 tentativas.
+Como sempre, lá no final do e-mail você poderá dar uma olhada na minha resolução do desafio de ontem.
+Sabe quando você vai no supermercado com uma lista de compras e acaba ficando indo e voltando nos mesmos corredores até completar a lista?
 
-No fim, caso você não acerte nenhuma vez, ele vai imprimir qual era o número inicial.
+Você precisa de uma maçã e vai para a área de frutas. O próximo item é um leite e você segue para os laticínios. Mas em seguida você anotou a pêra, e precisa novamente retornar para a área de frutas.
 
-Depois que o programinha estiver funcionando, tente usar um número randômico em vez de um número pré-definido.
+Depois que você resolver o desafio de hoje, com certeza não fará mais isso!
+
+Assim como a nossa lista de compras, é muito comum que programas trabalhem com listas de strings, números e objetos.
+
+Pense em todo catálogo de e-commerce que você já viu, na lista de eventos do seu Google Calendar, ou ainda na sua caixa de e-mails. Todos esses sites usam listas para exibir informações de uma forma simples e fácil de entender.
+
+Além disso, você pode aproveitar as listas para fazer filtros, ordenação, e outras funcionalidades muito úteis.
+
+Nesse ponto, você já deve ter percebido que trabalhar com essas coleções é algo que você vai precisar dominar, né?
+
+Então hoje, para facilitar a sua ida ao supermercado, você deve criar um programa em Javascript que perguntará se você deseja adicionar uma comida na sua lista de compras, e você deve poder responder com sim ou não.
+
+Em seguida, ele perguntará qual comida você deseja inserir, e você digitará o nome dela, como por exemplo batata.
+
+Depois, ele deverá perguntar em qual categoria essa comida se encaixa, com algumas opções já pré-definidas, como frutas, laticínios, congelados, doces e o que mais você achar interessante. Assim, você poderá separar tudo no seu devido grupo.
+
+Por fim, caso você não queira mais adicionar nada na lista de compras e responder não na primeira pergunta, ele irá exibir uma lista com todos os itens agrupados, da seguinte forma:
+
+Caso você adicione na sua lista:
+banana, leite em pó, tomate, leite vegetal, chiclete, bala de ursinho, maçã, uva, abacate e leite de vaca
+
+O programa deverá imprimir, por exemplo:
+
+Lista de compras:
+    Frutas: banana, tomate, maçã, uva, abacate
+    Laticínios: leite vegetal, leite de vaca, leite em pó
+    Congelados:
+    Doces: chiclete e bala de ursinho
 DICA
-Pense muito bem em qual estrutura de repetição você irá utilizar para fazer o seu programa ser executado até o momento em que as 3 tentativas acabem, ou até a pessoa acertar o número.
+Existe um objeto dentro da linguagem Javascript que é usado justamente para criar listas de elementos, chamado Array. Use e abuse dele!
 
-Lembre-se que você pode sempre personalizar o seu programa da forma que quiser.
+Lembre-se que você sempre pode estilizar da maneira que quiser o seu programinha, inclusive utilizando outras tecnologias para isso, como HTML e CSS.
 
-Não se esqueça de compartilhá-lo no seu GitHub e nas suas redes sociais com a hashtag #7DaysOfCode, e também com #feedback7DoC caso você precise de alguma ajuda.
+Porém, isso não é obrigatório na nossa lista de lógica de programação com Javascript. Como eu já mencionei nos dias anteriores, você pode usar recursos como console.log, alert e prompt para desenvolver o seu programa.
+
+Não se esqueça de compartilhar o seu código no seu GitHub e nas suas redes sociais com a hashtag #7DaysOfCode, e também com #feedback7DoC caso você precise de alguma ajuda.
 EXTRA
-Dê uma olhada nesse site para aprender mais sobre estruturas de repetição.
+Para criar um array vazio, você pode usar colchetes. E aí, para inserir algo em um array, você pode usar a função .push(). Por exemplo:
 
-Para fazer a própria máquina escolher sozinha o número a ser adivinhado, você pode utilizar algo chamado Math.random().
+let meuArray = [];
+meuArray.push(elemento1);
 
-Para isso, utilize o código:
+Depois disso, o array não estará mais vazio, ele terá o elemento1.
 
-Math.floor(Math.random() * (máximo - mínimo + 1) + mínimo)
+Para aprender mais sobre arrays em Javascript, dê uma olhada nesse site. */
 
-Onde, claro, você terá que alterar os valores de mínimo e máximo pelos limites inferior e superior, respectivamente.
 
-Você pode ler mais sobre a Math.random() nesse site.
-*/
-
-const numeroAdivinhacao = Math.floor(Math.random() * (10 - 0 + 1) + 0);
-let chute = "";
-let acertou = false;
-
-for(let contador = 0; contador < 3; contador++){    
-    chute = prompt("Tente adivinhar o número de 0 a 10:");
-    if(chute == numeroAdivinhacao){
-        alert(`Parabéns, você acertou! O número era ${numeroAdivinhacao}.`);
-        acertou = true;
-        break;
-    }
-    alert("Errado!");
-}
-if(!acertou){
-    alert(`Infelizmente, você não acertou. O número era ${numeroAdivinhacao}!`);
-}
